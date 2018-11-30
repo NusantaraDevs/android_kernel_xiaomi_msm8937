0d992481dac4765adae76f10a4d00dc56acb62db
Zulfikarbot <taitowatashiwa1@gmail.com>
2018-11-30 09:54:56 +0700

    Cleanup

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Fri Nov 30 09:49:43 WIB 2018"
-#define LINUX_COMPILE_BY "AyraHikari"
-#define LINUX_COMPILE_HOST "NusantaraDevs"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
